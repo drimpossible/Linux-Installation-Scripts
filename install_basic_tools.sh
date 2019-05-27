f4ec7415c5e8b598165eda6c65356e1b135fddb5
Bayesian Conspirator <bayesianconspirator@mailfence.com>
2019-05-26 20:57:56 -0400

    Minor modifications

--- install_basic_tools.sh
+++ install_basic_tools.sh
@@ -1,6 +1,6 @@
-sudo apt -y update && sudo apt -y upgrade  
-sudo apt -y install build-essential cmake g++ gcc gfortran git pkg-config python3-dev software-properties-common wget tmux ncdu htop gparted dkms linux-headers-generic libncurses-dev libboost-all-dev
-sudo apt -y autoremove 
+sudo apt update && sudo apt upgrade  
+sudo apt install -y build-essential cmake g++ gcc gfortran git pkg-config python3-dev software-properties-common wget tmux ncdu htop gparted dkms linux-headers-generic libncurses-dev libboost-all-dev
+sudo apt autoremove 
 sudo rm -rf /var/lib/apt/lists/*
 
 # Install OpenBLAS
@@ -16,6 +16,13 @@ sudo apt -y install zsh
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
+# Install CUDA
+wget -c https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
+sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
+sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
+sudo apt -y update
+sudo apt -y install cuda
 
 sudo apt install -y libfreetype6-dev libpng12-dev texlive-full texmaker openssh-server libhdf5-dev
 sudo apt remove -y mono-* libmono-* xscreensaver-gl
--- pip3_install.sh
+++ pip3_install.sh
@@ -1,9 +1,13 @@
 # Install Anaconda
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 bash Anaconda3-2019.03-Linux-x86_64.sh
-conda create --name 3 python=3
+source ~/.bashrc
+conda create --name 3 python=3 -y
 conda activate 3
-conda install numpy scipy matplotlib ipython jupyter pandas sympy nose
+conda install numpy scipy matplotlib ipython jupyter pandas sympy nose -y
 conda uninstall -y --force pillow pil jped libtiff
 conda install -yc conda-forge libjpeg-turbo
 conda install pillow-simd
+conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
+
+
