4770632147a99a9e0a3c6a9b33c578f270eca0e0
anonymous <anonymous>
2019-05-29 12:12:53 -0400

    Add

--- basic_install_full.sh
+++ basic_install_full.sh
@@ -1,6 +1,6 @@
-sudo apt update && sudo apt upgrade  
+sudo apt update -y  && sudo apt upgrade -y
 sudo apt install -y build-essential cmake g++ gcc gfortran git pkg-config python3-dev software-properties-common wget tmux ncdu htop gparted dkms linux-headers-generic libncurses-dev libboost-all-dev
-sudo apt autoremove 
+sudo apt autoremove -y 
 sudo rm -rf /var/lib/apt/lists/*
 
 # Install OpenBLAS
@@ -12,7 +12,7 @@ make FC=gfortran -j $(($(nproc) + 1))
 sudo make PREFIX=/usr/local install
 
 # Install ZSH
-sudo apt -y install zsh
+sudo apt install -y zsh
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -21,11 +21,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 wget -c https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
 sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
 sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
-sudo apt -y update
-sudo apt -y install cuda
+sudo apt update -y 
+sudo apt install -y cuda
 
 sudo apt install -y libfreetype6-dev libpng12-dev texlive-full texmaker openssh-server libhdf5-dev
 sudo apt remove -y mono-* libmono-* xscreensaver-gl
 sudo ufw enable
 
+sudo reboot
 
