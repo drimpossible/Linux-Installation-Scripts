43bb4bc39b22d2a81edd0178e45e479dcd9209c1
Ameya Prabhu <8457628+drimpossible@users.noreply.github.com>
2019-06-04 13:47:12 +0000

    Update basic_install_full.sh

--- basic_install_full.sh
+++ basic_install_full.sh
@@ -23,6 +23,12 @@ sudo apt-get remove mono-runtime-common gnome-orca
 # Install slack
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
 sudo apt install ./slack-desktop-*.deb
+rm ./slack-desktop-3.3.8-amd64.deb
+
+# Install Discord
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo apt install --fix-broken ./discord.deb
+rm ./discord.deb
 
 # Install OpenBLAS
 cd ~/installs
