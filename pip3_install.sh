7c6f3be5fdf1883ac686ac3d8fb66171cd950536
Ameya Prabhu <ameya.prabhu@research.iiit.ac.in>
2019-07-15 21:02:31 +0530

    Update pip3_install.sh

--- pip3_install.sh
+++ pip3_install.sh
@@ -23,6 +23,6 @@ pip3 install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp36-cp36m-linux
 pip3 install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp36-cp36m-linux_x86_64.whl
 pip3 install gpytorch torchtext fastai
 pip3 install torchsummary
-pip3 install botorch
+pip3 install botorch ax-platform
 
 
