51ce50cf9726722cba7064d8550cdfe4fabf0ca3
Ameya Prabhu <drimpossible@mailfence.com>
2019-12-28 17:53:47 +0000

    Create snap_packages.sh

---
+++ snap_packages.sh
@@ -0,0 +1,9 @@
+sudo snap install code --classic
+sudo snap install discord
+sudo snap install slack --classic
+sudo snap install ffmpeg
+sudo snap install vlc
+sudo snap install tor
+sudo snap install okular
+sudo snap install tusk
+sudo snap install mumble
